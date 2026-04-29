9f3293ef746ba1c0e7f01d1f33f8415108efcf2d
Decoded_191 <suraj00949@gmail.com>
2026-04-29 08:28:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@ RED='\033[1;91m'
 BLUE='\033[1;94m'
 RESET='\033[0m'
 
+ping -c 1 google.com >/dev/null 2>&1
+if [ "$?" != '0' ]; then
+  printf "Internet on karlo please!!\n"
+  exit 1
+fi 
+
 echo -e "${BLUE}[*]Installing Ruby${RESET}"
 pkg install ruby||{
 echo -e "${RED}[✗]Downloading Failed${RESET}"
